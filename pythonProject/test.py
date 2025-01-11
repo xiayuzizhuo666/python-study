bb2251d616bf34b85663d60df3b160c4bd328a36
xiayuzizhuo <2964296252@qq.com>
2025-01-11 22:50:27 +0800

    2025.1.11
    入门

---
+++ pythonProject/test.py
@@ -0,0 +1,55 @@
+# print("Hello World")
+
+# a = "我是帅哥"
+# print(a)
+
+# a = 3.14
+# b = 6.28
+# c = a + b
+# print(c)
+# # 查看数据类型
+# print(type(c))
+#
+# # 数据类型转换
+# c = int(c)
+# print(c)
+# print(type(c))
+#
+# a = 2**4
+# print(a)
+# print(type(a))
+# b = 5//2
+# print(b)
+# print(type(b))
+# c = 5%2
+# print(c)
+# print(type(c))
+
+
+# a = '帅哥'
+# print(a)
+# b = "美女"
+# print(b)
+# c = """帅哥
+# 美女     爱对方
+# 一辈子"""
+# print(c)
+# # 三引号可以保存格式进行输出
+# d = a + b
+# print(d)
+
+# class_name = input("Enter a class name: ")
+# avq_salary = int(input("Enter a salary: "))
+# print("第%s期学员平均月薪：%s" % (class_name,avq_salary))
+# num1 = 11.345
+# num2 = 45.25654
+# print("%7.2f" % num1)
+# print("%.2f" % num2)
+
+# class_name = input("Enter a class name: ")
+# avq_salary = int(input("Enter a salary: "))
+# print(f"第{class_name}期学员平均月薪：{avq_salary}" )
+
+print('1*1的结果：%d'% (1 * 1))
+print(f"1 * 1结恶果是：{1 * 1}")
+print("字符的数据类型：%s" %(type('字符串')))
