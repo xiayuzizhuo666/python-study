368716d90cfc9b1fa18c7a4fa18663dcb912d488
xiayuzizhuo <2964296252@qq.com>
2025-01-14 22:22:02 +0800

    study

---
+++ 2025.1.14/pythonProject1/test.py
@@ -0,0 +1,38 @@
+# t1 = (1,"Hello",True)
+# t2 = ()
+# t3 = tuple()
+# print(t1)
+# print(t2)
+# print(t3)
+
+
+# tup = ( "xiayu" ,2 ,"zizhuo" ,4 ,5)
+# print(tup.index(("xiayu")))
+# print(tup.count("zizhuo"))
+# print(len(tup))
+#
+# str = "hello and good"
+# print(str.index("hello"))
+
+# my_string = "Hello Python"
+# my_string = my_string.split(" ")
+# print(my_string)
+
+
+# str = input("Enter a string: ")
+# new_str = str[::2]
+# print(new_str)
+
+
+# set = {1,2,3}
+# print(set)
+# set.add(4)
+# print(set)
+# set.remove(4)
+# print(set)
+
+
+set1 = {1 ,2,3}
+set2 = {4,5,6}
+set3 = set1.difference(set2)
+print(set3)
